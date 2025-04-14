affac617ea6b83ceaef989cd86738fbd09826b98
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-04-15 07:39:01 +0800

    Count and Contains excercise

--- CodingPractice/AnyAll.cs
+++ CodingPractice/AnyAll.cs
@@ -39,5 +39,47 @@ namespace LinqPractice.CodingPractice
 
             return hasWhiteSpace;
         }
+
+
+        /*
+         * Count & Contains
+         * 
+         * Implement the CountListsContainingZeroLongerThan method. It takes the following parameters:
+         * 
+         * - length (int): the minimum length a list must exceed
+         * - listsOfNumbers (List<List<int>>): a list of integer lists
+         * 
+         * This method should return the count of the lists that meet **both** of the following conditions:
+         * 
+         * 1. Contain the number zero
+         * 2. Are longer than the specified length
+         * 
+         * Example:
+         * ----------
+         * Input:
+         *   length = 3
+         *   listsOfNumbers =
+         *   {
+         *       {1, 2, 5, -1},
+         *       {0, 4, 4, 6},
+         *       {9, 0}
+         *   }
+         * 
+         * Output:
+         *   1
+         * 
+         * Explanation:
+         *   Only the second list contains the number zero and has more than 3 items.
+         */
+
+        public static int CountListsContainingZeroLongerThan(
+            int length,
+            List<List<int>> listsOfNumbers)
+        {
+            var countListcontainNumberZeroWithLongerThanLength
+                = listsOfNumbers.Count(list => list.Contains(0) && list.Count() > length);
+
+            return countListcontainNumberZeroWithLongerThanLength;
+        }
     }
 }
